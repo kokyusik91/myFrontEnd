3868638bd5ab71d5c45d45e9113d837afdae8d51
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2021-10-29 15:49:22 +0900

    App.js 최신화

--- velopart_react/src/App.js
+++ velopart_react/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Button from './Button';
 import styled, { ThemeProvider } from 'styled-components';
 import Dialog from './Dialog';
+import CounterHook from './CounterHook';
+import Say from './Say';
 
 //app component에서 색상 선언후 어떤 컴포넌트에서도 쓸 수 있게 만든다.
 //1. ThemeProvider를 import 해온후
@@ -24,46 +26,47 @@ const palette = {
 
 function App() {
   return (
-    <ThemeProvider theme={{ palette }}>
-      <AppBlocking>
-        <ButtonGroup>
-          <Button color='pink' size='large'>
-            BUTTON
-          </Button>
-          <Button>BUTTON</Button>
-          <Button color='gray' size='small'>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color='pink' size='large' outline>
-            BUTTON
-          </Button>
-          <Button outline>BUTTON</Button>
-          <Button color='gray' size='small' outline>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color='pink' size='large' fullwidth>
-            BUTTON
-          </Button>
-          <Button fullwidth size='large'>
-            BUTTON
-          </Button>
-          <Button color='gray' size='large' fullwidth>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        {/* <Dialog
-          title='정말로 삭제하시겠습니까?'
-          comfirmText='삭제'
-          cancelText='취소'
-        >
-          데이터를 정말로 삭제하시겠습니까?
-        </Dialog> */}
-      </AppBlocking>
-    </ThemeProvider>
+    <Say />
+    // <ThemeProvider theme={{ palette }}>
+    //   <AppBlocking>
+    //     <ButtonGroup>
+    //       <Button color='pink' size='large'>
+    //         BUTTON
+    //       </Button>
+    //       <Button>BUTTON</Button>
+    //       <Button color='gray' size='small'>
+    //         BUTTON
+    //       </Button>
+    //     </ButtonGroup>
+    //     <ButtonGroup>
+    //       <Button color='pink' size='large' outline>
+    //         BUTTON
+    //       </Button>
+    //       <Button outline>BUTTON</Button>
+    //       <Button color='gray' size='small' outline>
+    //         BUTTON
+    //       </Button>
+    //     </ButtonGroup>
+    //     <ButtonGroup>
+    //       <Button color='pink' size='large' fullwidth>
+    //         BUTTON
+    //       </Button>
+    //       <Button fullwidth size='large'>
+    //         BUTTON
+    //       </Button>
+    //       <Button color='gray' size='large' fullwidth>
+    //         BUTTON
+    //       </Button>
+    //     </ButtonGroup>
+    //     {/* <Dialog
+    //       title='정말로 삭제하시겠습니까?'
+    //       comfirmText='삭제'
+    //       cancelText='취소'
+    //     >
+    //       데이터를 정말로 삭제하시겠습니까?
+    //     </Dialog> */}
+    //   </AppBlocking>
+    // </ThemeProvider>
   );
 }
 
