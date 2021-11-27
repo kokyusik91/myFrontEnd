6eb00c572f612dfdda5cf90ee7061763f75ded87
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2021-11-27 17:11:28 +0900

    수정

--- redux-practice/src/components/LifeCycle.js
+++ redux-practice/src/components/LifeCycle.js
@@ -10,6 +10,8 @@ function LifeCycle() {
   return (
     <div>
       <p>You clicked {count} times</p>
+      <p>You clicked {count} times</p>
+      <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
