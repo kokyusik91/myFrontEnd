58a25b170199e1d46cfe72948503e33515fb4c52
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2021-12-23 16:33:58 +0900

    프로그래머스 알고리즘 풀이

--- JS_algorithm/hanghae99/챕터2/b10773/app.js
+++ JS_algorithm/hanghae99/챕터2/b10773/app.js
@@ -1,36 +1,36 @@
-// /* 백준 용 */
-// // const fs = require('fs');
-// // let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-
+/* 백준 용 */
 // const fs = require('fs');
-// const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-// const input = fs
-//   .readFileSync(filePath)
-//   .toString()
-//   .split('\n')
-//   .map((el) => +el);
-// const array = [];
-// for (let i = 1; i < input.length; i++) {
-//   array.push(input[i]);
-// }
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-// console.log(array);
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split('\n')
+  .map((el) => +el);
+const array = [];
+for (let i = 1; i < input.length; i++) {
+  array.push(input[i]);
+}
 
-// function solution(array) {
-//   let answer;
-//   let stack = [];
+console.log(array);
 
-//   for (let x of array) {
-//     if (x === 0) stack.pop();
-//     else stack.push(x);
-//   }
+function solution(array) {
+  let answer;
+  let stack = [];
 
-//   answer = stack.reduce((a, b) => a + b, 0);
-//   console.log(answer);
-// }
+  for (let x of array) {
+    if (x === 0) stack.pop();
+    else stack.push(x);
+  }
 
-// /* 알고리즘 강의용 */
-// solution(array);
+  answer = stack.reduce((a, b) => a + b, 0);
+  console.log(answer);
+}
+
+/* 알고리즘 강의용 */
+solution(array);
 
 setTimeout(
   function (name) {
