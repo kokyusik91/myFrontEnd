6f4630a6b3c9e6f92498298d1d887ee512a10d3b
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2022-01-13 23:14:45 +0900

    웹팩 샛팅 방법

---
+++ Learn_webpack/code-splitting/webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'none', // mode에는 3가지 종류가 있다. production, development, none
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js', //'[name][chunkhash].js'
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
