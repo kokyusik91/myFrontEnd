78572cbf13b65775b7298b5ce7617d9ee95f4a90
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2022-01-20 22:59:49 +0900

    타입스크립트 적립

--- Learn_typescript/TypeScript/index.js
+++ Learn_typescript/TypeScript/index.js
@@ -9,16 +9,16 @@ var 옵젝트 = { name: 'kim' };
 // 함수의 타입지정
 // 인자로는 무조건 number타입을 받고 리턴값도 number로 리턴한다.
 function 함수(x) {
-    return x * 2;
+  return x * 2;
 }
 var john = { name: 'ko' };
 var adam = { name: 'ko', age: '123' };
 var project = {
-    member: ['kim', 'park'],
-    days: 30,
-    started: true,
+  member: ['kim', 'park'],
+  days: 30,
+  started: true,
 };
-//타입을 미리 지정하기 어려울때 (2개이상의 타입) union type이라고 한다.
+//타입을 미리 지정하기 어려울때 (2개이상의 타입) union type이라고 한다. 유니온 타입은 새로운 타입이다
 var 회원들 = [1, '2', 3];
 // a라는 key를 가진 프로퍼티는 문자열, 숫자가 올 수 있다.
 var 오브젝트 = { a: '123' };
@@ -33,9 +33,9 @@ var married = false;
 // 이런 개같은 경우가
 var 철수 = [user, age, married];
 var 학교 = {
-    score: [100, 97, 84],
-    teacher: 'Phil',
-    friend: 'John',
+  score: [100, 97, 84],
+  teacher: 'Phil',
+  friend: 'John',
 };
 학교.score[4] = false;
 학교.friend = ['Lee', 학교.teacher];
