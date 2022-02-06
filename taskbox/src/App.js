2f49d85254eb89fbc228e5e92d98f5e8f796add5
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2022-02-06 22:48:44 +0900

    리액트 스토리북 적용중

---
+++ taskbox/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Task from './components/Task';
+import { Usage } from './components/usage';
+
+function App() {
+  return (
+    <div className='App'>
+      <Usage />
+    </div>
+  );
+}
+
+export default App;
