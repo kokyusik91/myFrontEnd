3c41fcb09a1925e7ba5f7ae5ee186853dc0afa25
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2022-02-24 23:08:21 +0900

    webpack config

--- Learn_webpack/code-splitting/webpack.config.js
+++ Learn_webpack/code-splitting/webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+// webpack config
 module.exports = {
   mode: 'none', // mode에는 3가지 종류가 있다. production, development, none
   entry: './index.js',
